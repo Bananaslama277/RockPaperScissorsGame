90c1d75711c39f05bb6a4dac6300e565f2c2d2b8
Bananaslama277 <tracesmith555@gmail.com>
2023-02-23 08:12:17 -0600

    Initial Commit

---
+++ RockPaperScissorsGame/RockPaperScissorsGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  RockPaperScissorsGameApp.swift
+//  RockPaperScissorsGame
+//
+//  Created by Trace Smith on 2/23/23.
+//
+
+import SwiftUI
+
+@main
+struct RockPaperScissorsGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
