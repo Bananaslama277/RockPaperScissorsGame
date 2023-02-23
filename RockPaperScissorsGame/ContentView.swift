bfe2bc0ecc3a5adf1870ac69eb8458b0ed12e8f7
Bananaslama277 <tracesmith555@gmail.com>
2023-02-23 08:46:32 -0600

    initial commit

--- RockPaperScissorsGame/ContentView.swift
+++ RockPaperScissorsGame/ContentView.swift
@@ -1,21 +1,118 @@
-//
-//  ContentView.swift
-//  RockPaperScissorsGame
-//
-//  Created by Trace Smith on 2/23/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            Color.blue.ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Text("ROCK PAPER SCISSORS GAME")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+                
+                Text("Select Rock, Paper, or Scissors to start the game")
+                    .font(.title)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    Button {
+                        print("Button Clicked")
+                    } label: {
+                        
+                        Text("🪨")
+                            .font(.system(size: 75))
+                    }
+                    
+                    Spacer()
+                    
+                    Button {
+                        print("Button Clicked")
+                    } label: {
+                        Text("📄")
+                            .font(.system(size: 75))
+                    }
+                    
+                    Spacer()
+
+                    Button {
+                        print("Button Clicked")
+                    } label: {
+                        Text("✂️")
+                            .font(.system(size: 75))
+                    }
+                    
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                HStack {
+                    Spacer()
+                    
+                    VStack{
+                        Text("🪨")
+                            .font(.system(size: 60))
+                            .padding(.bottom, 10.0)
+
+                        Text("Player")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+                        
+                        Text("0")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+
+                    }
+                    
+                    Spacer()
+                    
+                    VStack{
+                        Text("🪨")
+                            .font(.system(size: 60))
+                            .padding(.bottom, 10.0)
+
+                        Text("CPU")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                            .padding(.bottom, 10.0)
+
+                        Text("0")
+                            .font(.title)
+                            .foregroundColor(Color.white)
+                    }
+                    
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                Button {
+                    print("Button Clicked")
+                } label: {
+                    Text("RESTART GAME")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color.white)
+                        .padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
+                        .border(/*@START_MENU_TOKEN@*/Color.white/*@END_MENU_TOKEN@*/, width: /*@START_MENU_TOKEN@*/7/*@END_MENU_TOKEN@*/)
+                        .cornerRadius(/*@START_MENU_TOKEN@*/10.0/*@END_MENU_TOKEN@*/)
+
+                }
+                
+            }
         }
-        .padding()
     }
 }
 
